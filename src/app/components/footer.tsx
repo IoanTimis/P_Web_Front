e599380d5fa93abecc5010e6069d46835e4e6770
lazea <alex.lazea01@e-uvt.ro>
2025-03-29 00:35:54 +0200

    Navbar and Footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white text-center py-4 mt-auto">
+            © 2025 My Monopoly Game. All rights reserved.
+        </footer>
+    );
+}
+
+export default Footer;
